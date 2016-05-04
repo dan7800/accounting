624bec16398217a03e95b25d7cfe954c64cbfc89
Keegan Parrotte <kmp3325@rit.edu>
2016-05-03 23:03:32 -0400

    Fixed liquibase stuff.

--- src/main/resources/migrations.sql
+++ src/main/resources/migrations.sql
@@ -1,13 +1,13 @@
 --liquibase formatted sql
 
---changeset:1
-create table `transactions` (
+--changeset keegan:1 dbms:mysql
+CREATE TABLE `transactions` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `timestamp` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) EGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-create table `entries` (
+CREATE TABLE `entries` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `transactionId` int(11) unsigned NOT NULL,
   `toAccountId` int(11) unsigned NOT NULL,
@@ -16,9 +16,8 @@ create table `entries` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-create table `accounts` (
+CREATE TABLE `accounts` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL
+  `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
