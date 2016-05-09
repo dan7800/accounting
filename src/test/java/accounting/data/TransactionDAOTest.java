60efb9dec726c60a9b13e2b809ee592f1ef2da1a
Keegan Parrotte <kmp3325@rit.edu>
2016-05-09 00:02:01 -0400

    Starting to write DAO tests.

--- src/test/java/accounting/data/TransactionDAOTest.java
+++ src/test/java/accounting/data/TransactionDAOTest.java
@@ -1,5 +1,23 @@
 package accounting.data;
 
-public class TransactionDAOTest {
+import org.junit.Before;
+import org.junit.Test;
+import org.skife.jdbi.v2.DBI;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TransactionDAOTest {
+    private TransactionDAO dao;
+
+    @Before
+    public void setup() {
+        DBI dbi = new DBI("jdbc:mysql://localhost:3306/accounting", "root", "root");
+        this.dao = dbi.onDemand(TransactionDAO.class);
+    }
+
+    @Test
+    public void insertTransaction() {
+        long id = dao.insertTransaction("hello");
+        assertThat(dao.get(id)).isNotNull();
+    }
 }
